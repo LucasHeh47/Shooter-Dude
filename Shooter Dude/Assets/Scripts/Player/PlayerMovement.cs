5732d95a4ceab974473e42ea0b607b90e565b6d0
LucasHeh47 <darthmonkey062005@gmail.com>
2020-12-30 21:14:13 -0800

    Update

--- Shooter Dude/Assets/Scripts/Player/PlayerMovement.cs
+++ Shooter Dude/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float MovementSpeed = 10;
+
     void Start()
     {
         
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + MovementSpeed * Time.fixedDeltaTime, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + -MovementSpeed * Time.fixedDeltaTime, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x + -MovementSpeed * Time.fixedDeltaTime, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + MovementSpeed * Time.fixedDeltaTime, transform.position.y, transform.position.z);
+        }
     }
 }
---
+++ Shooter Dude/Assets/Scripts/Scriptable Objects/Gun.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+public class Gun : ScriptableObject
+{
+
+    public Sprite GunImage;
+    public string GunName;
+
+    // COMMON
+    public float CommonDamage;
+    public float CommonFireRate;
+    public float CommonRange;
+
+    // UNCOMMON
+    public float UnCommonDamage;
+    public float UnCommonFireRate;
+    public float UnCommonRange;
+
+    // RARE
+    public float RareDamage;
+    public float RareFireRate;
+    public float RareRange;
+
+    // EPIC
+    public float EpicDamage;
+    public float EpicFireRate;
+    public float EpicRange;
+
+    // LEGENDARY
+    public float LegendaryDamage;
+    public float LegendaryFireRate;
+    public float LegendaryRange;
+
+}
